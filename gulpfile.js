3ce7ac9e0ceff2ff990e8ef136db69a84803c3e2
ldagner <through.a.haze@gmail.com>
2015-08-15 01:32:46 +0300

    development template

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// directories
+var devDir = 'app/';
+var distDir = 'dist/';
+var sassDir = devDir + 'scss/';
+
+// requires
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer-core');
@@ -19,69 +25,33 @@ var spritesmith = require('gulp.spritesmith');
 gulp.task('serve', ['css'], function() {
 
     browserSync.init({
-        server: "./"
+        server: devDir
     });
 
-    gulp.watch("app/scss/sass/*.scss", ['css']);
-    gulp.watch("app/img/*", ['imagemin']);
-    gulp.watch("app/js/*.js", ['js']).on('change', browserSync.reload);
-    gulp.watch(["app/*.html"]).on('change', browserSync.reload);
-});
-
-// Compile sass into CSS
-gulp.task('compass', ['sprite'], function() {
-    return gulp.src('app/scss/*.scss')
-        .pipe(compass({
-            config_file: 'app/scss/config.rb',
-            sass: 'scss',
-            comments: true,
-            sourcemap: true
-        }))
-        .pipe(gulp.dest('app/sass_css'))
-        .pipe(browserSync.stream());
-});
-
-// Autoprefix and minify css file & auto-inject into browsers
-gulp.task('css', ['compass'], function () {
-    return gulp.src(['app/sass_css/*.css'])
-        .pipe(postcss([ autoprefixer({ browsers: ['> 1%'], cascade: false }) ]))
-        .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(minifyCss())
-        .pipe(sourcemaps.write())
-        .pipe(rename('bundle.min.css'))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.stream());
-});
-
-// Concat all .js files and uglify it
-gulp.task('js', function () {
-    return gulp.src(['app/js/vendor/jquery-1.11.2.min.js', 'app/js/vendor/*.js', 'app/js/*.js'])
-        .pipe(sourcemaps.init())
-            .pipe(concat('all.js'))
-            .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(rename('scripts.min.js'))
-        .pipe(gulp.dest('app/js'));
+    gulp.watch([sassDir + '*.scss', sassDir + 'sass/*.scss'], ['css']);
+    gulp.watch(devDir + 'img/*', ['imagemin']);
+    gulp.watch(devDir + 'js/*.js', ['js']).on('change', browserSync.reload);
+    gulp.watch([devDir + '*.html']).on('change', browserSync.reload);
 });
 
 // Image minimization
 gulp.task('imagemin', function () {
-    return gulp.src('app/img/*')
+    return gulp.src(devDir + 'img/src/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('app/img'));
+        .pipe(gulp.dest(devDir + 'img'));
 });
 
 // Generating sprite
 gulp.task('sprite', ['imagemin'], function () {
     // Generate our spritesheet
-    var spriteData = gulp.src('app/img/*.*')
+    var spriteData = gulp.src(devDir + 'img/*.*')
         .pipe(spritesmith({
             imgName: 'sprite.png',
-            imgPath: '/app/img/sprite/sprite.png',
+            imgPath: devDir + '/img/sprite/sprite.png',
             cssName: '_sprite.scss',
             padding: 2,
             algorithm: 'left-right',
@@ -93,19 +63,56 @@ gulp.task('sprite', ['imagemin'], function () {
     // Pipe image stream through image optimizer and onto disk
     var imgStream = spriteData.img
         .pipe(imagemin())
-        .pipe(gulp.dest('app/img/sprite'));
+        .pipe(gulp.dest(devDir + 'img/sprite'));
 
     // Pipe CSS stream through CSS optimizer and onto disk
     var cssStream = spriteData.css
-        .pipe(gulp.dest('app/scss/sass'));
+        .pipe(gulp.dest(devDir + 'scss/sass'));
 
     // Return a merged stream to handle both `end` events
     return merge(imgStream, cssStream);
 });
 
+// Compile sass into CSS
+gulp.task('compass', ['sprite'], function() {
+    return gulp.src(sassDir + '*.scss')
+        .pipe(compass({
+            config_file: sassDir + 'config.rb',
+            css: devDir + 'css/src',
+            sass: sassDir,
+            comments: true,
+            sourcemap: true
+        }))
+        .pipe(gulp.dest(devDir + 'css/src'))
+        .pipe(browserSync.stream());
+});
+
+// Autoprefix and minify css file & auto-inject into browsers
+gulp.task('css', ['compass'], function () {
+    return gulp.src([devDir + 'css/src/*.css'])
+        .pipe(postcss([ autoprefixer({ browsers: ['> 1%'], cascade: false }) ]))
+        .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(minifyCss())
+        .pipe(sourcemaps.write())
+        .pipe(rename('bundle.min.css'))
+        .pipe(gulp.dest(devDir + 'css'))
+        .pipe(browserSync.stream());
+});
+
+// Concat all .js files and uglify it
+gulp.task('js', function () {
+    return gulp.src([devDir + 'js/src/vendor/jquery-1.11.2.min.js', devDir + 'js/src/vendor/*.js', devDir + 'js/src/*.js'])
+        .pipe(sourcemaps.init())
+            .pipe(concat('all.js'))
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest(devDir + 'js'));
+});
+
 gulp.task('watch', function() {
-    gulp.watch(['app/css/*.css'], ['css']);
-    gulp.watch(['app/js/*.js'], ['js']);
+    gulp.watch([devDir + 'css/*.css'], ['css']);
+    gulp.watch([devDir + 'js/*.js'], ['js']);
 });
 
 // Build
@@ -130,4 +137,4 @@ gulp.task('build', ['clean', 'sprite', 'scss'], function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['css']);
+gulp.task('default', ['css', 'js']);
